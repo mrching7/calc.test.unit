56326cea2bf1e2c54edcba6ce377edb0411db498
zabih <44346668+mrching7@users.noreply.github.com>
2019-08-29 12:26:14 +0200

    Add project files.

---
+++ calc.test.unit/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace calc.test.unit
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        [Test]
+        public void Add_double()
+        {
+            //arrange
+            var uut= new Calc();
+            //act
+            uut.Add(2, 2);
+            //assert
+            Assert.That(uut.Add(2,2), Is.EqualTo(5));
+        }
+
+
+
+    }
+}
---
+++ calc/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public class Calc
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a,b);
+        }
+    }
+}
